6c8e6333ea622fc0d4291b7a1449f4643fa79b09
Duy Duong <duy98bn@gmail.com>
2024-03-30 16:25:26 +0700

    Add favorites

--- src/components/User/CreateArticle.jsx
+++ src/components/User/CreateArticle.jsx
@@ -1,9 +1,168 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import Footer from "../Footer";
 
 const CreateArticle = () => {
-  return (
-    <div>CreateArticle</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    articleTitle: "",
+    description: "",
+    body: "",
+    tags: "",
+  });
 
-export default CreateArticle
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch("https://api.realworld.io/api/articles", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${localStorage.getItem("auth-token")}`,
+        },
+        body: JSON.stringify({
+          article: {
+            title: formData.articleTitle,
+            description: formData.description,
+            body: formData.body,
+            tagList: formData.tags.split(",").map((tag) => tag.trim()),
+          },
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to create article");
+      }
+  
+      const createdArticle = await response.json();
+      console.log("Article created:", createdArticle);
+  
+      setFormData({
+        articleTitle: "",
+        description: "",
+        body: "",
+        tags: "",
+      });
+    } catch (error) {
+      console.error("Error creating article:", error);
+    }
+  };
+
+  return (
+    <div className="editor-page">
+      <Container style={{ marginTop: "1.5rem" }}>
+        <div className="row">
+          <div className="col-md-10 offset-md-1 col-xs-12">
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset
+                  className="form-group"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <input
+                    type="text"
+                    name="articleTitle"
+                    className="form-control form-control-lg"
+                    placeholder="Article Title"
+                    value={formData.articleTitle}
+                    onChange={handleChange}
+                    style={{
+                      padding: ".75rem 1.5rem",
+                      fontSize: "1.25rem",
+                      borderRadius: ".3rem",
+                    }}
+                  />
+                </fieldset>
+                <fieldset
+                  className="form-group"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <input
+                    type="text"
+                    name="description"
+                    className="form-control"
+                    placeholder="What's this article about?"
+                    value={formData.description}
+                    onChange={handleChange}
+                    style={{
+                      padding: ".75rem 1.5rem",
+                      fontSize: "1.25rem",
+                      borderRadius: ".3rem",
+                    }}
+                  />
+                </fieldset>
+                <fieldset
+                  className="form-group"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <textarea
+                    className="form-control"
+                    name="body"
+                    rows="8"
+                    placeholder="Write your article (in markdown)"
+                    value={formData.body}
+                    onChange={handleChange}
+                    style={{
+                      padding: ".75rem 1.5rem",
+                      fontSize: "1.25rem",
+                      borderRadius: ".3rem",
+                    }}
+                  ></textarea>
+                </fieldset>
+                <fieldset
+                  className="form-group"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  <input
+                    type="text"
+                    name="tags"
+                    className="form-control"
+                    placeholder="Enter tags"
+                    value={formData.tags}
+                    onChange={handleChange}
+                    style={{
+                      padding: ".75rem 1.5rem",
+                      fontSize: "1.25rem",
+                      borderRadius: ".3rem",
+                    }}
+                  />
+                  <div className="tag-list"></div>
+                </fieldset>
+                <button
+                  className="btn btn-lg pull-xs-right btn-primary"
+                  type="submit"
+                  style={{
+                    float: "right",
+                    padding: ".75rem 1.5rem",
+                    fontSize: "1.25rem",
+                    borderRadius: ".3rem",
+                    backgroundColor: "#5cb85c",
+                    borderColor: "#5cb85c",
+                    display: "inline-block",
+                    fontWeight: "400",
+                    lineHeight: "1.25",
+                    textAlign: "center",
+                    whiteSpace: "nowrap",
+                    verticalAlign: "middle",
+                    cursor: "pointer",
+                    userSelect: "none",
+                    border: "1px solid transparent",
+                  }}
+                >
+                  Publish Article
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default CreateArticle;
--- src/components/User/Favorite.jsx
+++ src/components/User/Favorite.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import Footer from "../Footer";
 import { Link, useParams } from "react-router-dom";
 
 const getUserProfileByUsername = async (username) => {
@@ -30,7 +29,7 @@ const getUserProfileByUsername = async (username) => {
 const getUserArticlesByUsername = async (username) => {
   try {
     const response = await fetch(
-      `https://api.realworld.io/api/articles?limit=5&offset=0&author=${username}`,
+      `https://api.realworld.io/api/articles?limit=5&offset=0&favorited=${username}`,
       {
         method: "GET",
         headers: {
@@ -51,7 +50,34 @@ const getUserArticlesByUsername = async (username) => {
   }
 };
 
-const UserProfile = () => {
+const toggleFavoriteArticle = async (slug, isFavorited) => {
+  try {
+    const method = isFavorited ? "DELETE" : "POST";
+    const response = await fetch(
+      `https://api.realworld.io/api/articles/${slug}/favorite`,
+      {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${localStorage.getItem("auth-token")}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to toggle favorite status");
+    }
+
+    const favoritedArticle = await response.json();
+    console.log("Article favorited:", favoritedArticle);
+    return favoritedArticle;
+  } catch (error) {
+    console.error("Error toggling favorite status:", error);
+    throw error;
+  }
+};
+
+const Favorite = () => {
   const { username } = useParams();
   const [userProfile, setUserProfile] = useState(null);
   const [userArticles, setUserArticles] = useState([]);
@@ -69,7 +95,11 @@ const UserProfile = () => {
     const fetchUserArticles = async () => {
       try {
         const articlesData = await getUserArticlesByUsername(username);
-        setUserArticles(articlesData);
+        const updatedArticles = articlesData.map((article) => ({
+          ...article,
+          favorited: true,
+        }));
+        setUserArticles(updatedArticles);
       } catch (error) {
         console.error("Error fetching user articles:", error);
       }
@@ -78,6 +108,22 @@ const UserProfile = () => {
     fetchProfile();
     fetchUserArticles();
   }, [username]);
+
+  const handleToggleFavorite = async (slug, isFavorited) => {
+    try {
+      const updatedArticle = await toggleFavoriteArticle(slug, isFavorited);
+      setUserArticles((prevUserArticles) => {
+        const index = prevUserArticles.findIndex(
+          (article) => article.slug === slug
+        );
+        const updatedArticles = [...prevUserArticles];
+        updatedArticles[index] = updatedArticle.article;
+        return updatedArticles;
+      });
+    } catch (error) {
+      console.error("Error toggling favorite:", error);
+    }
+  };
 
   return (
     <div className="profile-page">
@@ -128,7 +174,7 @@ const UserProfile = () => {
                       className="bi bi-gear-wide"
                       viewBox="0 0 16 16"
                     >
-                      <path d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z" />
+                      <path d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l-.205.211a.96.96 0 0 1 1.622-.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z" />
                     </svg>{" "}
                     Edit Profile Settings
                   </Link>
@@ -154,12 +200,14 @@ const UserProfile = () => {
                     <li className="nav-item" style={{ float: "left" }}>
                       <Link
                         className="nav-link active"
-                        to={""}
+                        to={`/profile/${username}`}
                         previewlistener="true"
                         style={{
-                          background: "#fff",
-                          borderBottom: "2px solid #5cb85c",
-                          color: "#5cb85c",
+                          borderRadius: "0",
+                          border: "none",
+                          borderBottom: "2px solid transparent",
+                          background: "0 0",
+                          color: "#aaa",
                         }}
                       >
                         My Articles
@@ -174,11 +222,9 @@ const UserProfile = () => {
                         to={`/profile/${username}/favorites`}
                         previewlistener="true"
                         style={{
-                          borderRadius: "0",
-                          border: "none",
-                          borderBottom: "2px solid transparent",
-                          background: "0 0",
-                          color: "#aaa",
+                          background: "#fff",
+                          borderBottom: "2px solid #5cb85c",
+                          color: "#5cb85c",
                         }}
                       >
                         Favorited Articles
@@ -205,7 +251,7 @@ const UserProfile = () => {
                       }}
                     >
                       <Link
-                        to={`/profile/${username}`}
+                        to={`/profile/${article.author.username}`}
                         previewlistener="true"
                         style={{
                           touchAction: "manipulation",
@@ -216,7 +262,7 @@ const UserProfile = () => {
                       >
                         <img
                           decoding="sync"
-                          src={userProfile.image}
+                          src={article.author.image}
                           alt="author avater"
                           style={{
                             display: "inline-block",
@@ -247,10 +293,10 @@ const UserProfile = () => {
                             backgroundColor: "transparent",
                           }}
                           className="author"
-                          to={`/profile/${username}`}
+                          to={`/profile/${article.author.username}`}
                           previewlistener="true"
                         >
-                          {userProfile.username}
+                          {article.author.username}
                         </Link>
                         <span
                           className="date"
@@ -272,32 +318,36 @@ const UserProfile = () => {
                           padding: ".25rem .5rem",
                           fontSize: ".875rem",
                           borderRadius: "0.2rem",
-                          color: "#5cb85c",
-                          backgroundImage: "none",
-                          backgroundColor: "transparent",
+                          color: article.favorited ? "#fff" : "#5cb85c",
+                          backgroundColor: article.favorited
+                            ? "#5cb85c"
+                            : "transparent",
                           borderColor: "#5cb85c",
                           display: "inline-block",
                           fontWeight: "400",
                           lineHeight: "1.25",
                           textAlign: "center",
-                          whiteSpace: "nowrap",
-                          verticalAlign: "middle",
                           cursor: "pointer",
                           userSelect: "none",
                           border: "1px solid #5cb85c",
                         }}
+                        onClick={() =>
+                          handleToggleFavorite(article.slug, article.favorited)
+                        }
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           width="16"
                           height="16"
-                          fill="currentColor"
+                          fill={article.favorited ? "#fff" : "currentColor"}
                           className="bi bi-suit-heart-fill"
                           viewBox="0 0 16 16"
                         >
                           <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
                         </svg>{" "}
-                        {article.favoritesCount}
+                        <span className="favorites-count">
+                          {article.favoritesCount}
+                        </span>
                       </button>
                     </div>
                     <Link
@@ -363,12 +413,20 @@ const UserProfile = () => {
                             key={tag}
                             className="tag-default tag-pill tag-outline"
                             style={{
-                              display: "inline",
+                              fontWeight: "300",
+                              fontSize: ".8rem",
+                              paddingTop: "0",
+                              paddingBottom: "0",
+                              border: "1px solid #ddd",
+                              color: "#aaa",
+                              background: "0 0",
+                              display: "inline-block",
+                              whiteSpace: "nowrap",
                               marginRight: "3px",
-                              background: "#5cb85c",
-                              color: "white",
-                              borderRadius: "10px",
-                              padding: "3px 6px",
+                              marginBottom: "0.2rem",
+                              paddingRight: ".6em",
+                              paddingLeft: ".6em",
+                              borderRadius: "10rem",
                             }}
                           >
                             {tag}
@@ -383,9 +441,59 @@ const UserProfile = () => {
           </Container>
         </>
       )}
-      <Footer />
+      <footer
+        className="footer position-absolute"
+        style={{
+          background: "#f3f3f3",
+          marginTop: "3rem",
+          padding: "1rem 0",
+          width: "100%",
+        }}
+      >
+        <Container>
+          <Link
+            className="logo-font"
+            to={"/home"}
+            previewlistener="true"
+            style={{
+              verticalAlign: "middle",
+              color: "#5cb85c",
+              textDecoration: "none",
+              fontWeight: "700",
+              backgroundColor: "transparent",
+            }}
+          >
+            conduit
+          </Link>
+          <span
+            className="attribution"
+            style={{
+              verticalAlign: "middle",
+              marginLeft: "10px",
+              fontSize: ".8rem",
+              color: "#bbb",
+              fontWeight: "300",
+            }}
+          >
+            An interactive learning project from{" "}
+            <Link
+              to={"https://thinkster.io"}
+              previewlistener="true"
+              style={{
+                touchAction: "manipulation",
+                color: "#5cb85c",
+                textDecoration: "none",
+                backgroundColor: "transparent",
+              }}
+            >
+              Thinkster
+            </Link>
+            . Code &amp; design licensed under MIT.
+          </span>
+        </Container>
+      </footer>
     </div>
   );
 };
 
-export default UserProfile;
+export default Favorite;
