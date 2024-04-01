9e1230d9c5fe519dd5fa32193c2a75e080b58597
Duy Duong <duy98bn@gmail.com>
2024-04-01 18:52:53 +0700

    fix profile favo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from "./components/HomePage";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import "bootstrap/dist/css/bootstrap.min.css";
-import HomePage from "./components/HomePage";
 import Setting from "./components/User/Setting";
 import UserProfile from "./components/User/UserProfile";
 import CreateArticle from "./components/User/CreateArticle";
@@ -19,15 +18,14 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" exact component={Home} element={<HomePage />} />
-          <Route path="/home" component={Home} element={<HomePage />} />
-          <Route path="/signin" component={SignIn} element={<SignIn />}/>
-          <Route path="/signup" component={SignUp} element={<SignUp />}/>
-          <Route path="/settings" component={Setting} element={<Setting />}/>
-          <Route path="/profile/:username" component={UserProfile} element={<UserProfile />}/>
-          <Route path="/profile/:username/favorites" component={Favorite} element={<Favorite />}/>
-          <Route path="/editor" component={CreateArticle} element={<CreateArticle />}/>
-          <Route path="/article/:slug" component={ArticleDetail} element={<ArticleDetail />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />}/>
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/settings" element={<Setting />}/>
+          <Route path="/profile/:username" element={<UserProfile />}/>
+          <Route path="/profile/:username/favorites" element={<Favorite />}/>
+          <Route path="/editor" element={<CreateArticle />}/>
+          <Route path="/article/:slug" element={<ArticleDetail />}/>
         </Routes>
       </div>
     </Router>
