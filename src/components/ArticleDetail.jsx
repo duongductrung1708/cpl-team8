5470e1261ebf71a6917511068da2d541f24998bf
NamDD182 <namddhe171565@fpt.edu.vn>
2024-04-03 09:06:48 +0700

    Namm done the articledetail edit and delete

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -18,6 +18,8 @@ const ArticleDetail = () => {
   const [comments, setComments] = useState([]);
   const [commentText, setCommentText] = useState("");
   const [postingComment, setPostingComment] = useState(false);
+  
+  const [isAuthor, setIsAuthor] = useState(false);
 
   const navigate = useNavigate();
 
@@ -37,6 +39,10 @@ const ArticleDetail = () => {
         setArticle(data.article);
         setIsFollowing(data.article.author.following);
         setIsFavorited(data.article.favorited);
+        setIsAuthor(
+          data.article.author.username === localStorage.getItem("auth-username")
+        );
+
         setLoading(false);
       } catch (error) {
         console.error("Error fetching article:", error);
@@ -62,6 +68,32 @@ const ArticleDetail = () => {
     fetchArticle();
     fetchComments();
   }, [slug]);
+
+  const handleEditArticle = () => {
+    navigate(`/editor/${slug}`, { state: { article } }); // Pass the article data to CreateArticle
+  };
+
+  const handleDeleteArticle = async () => {
+    try {
+      const response = await fetch(
+        `https://api.realworld.io/api/articles/${slug}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Token ${localStorage.getItem("auth-token")}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to delete article");
+      }
+
+      navigate("/home");
+    } catch (error) {
+      console.error("Error deleting article:", error);
+    }
+  };
 
   const formatDate = (dateString) => {
     const options = { month: "long", day: "numeric", year: "numeric" };
@@ -220,51 +252,96 @@ const ArticleDetail = () => {
                   </Link>
                   <span className="date">{formatDate(article.createdAt)}</span>
                 </div>
-                <button
-                  className="btn btn-sm action-btn btn-outline-secondary"
-                  onClick={toggleFollow}
-                  disabled={!isLoggedIn || isFollowingLoading}
-                >
-                  <i className="ion-plus-round"></i>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-plus-lg"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
-                    />
-                  </svg>
-                  {isFollowing ? "Unfollow" : "Follow"}{" "}
-                  {article.author.username}
-                </button>{" "}
-                &nbsp;
-                <button
-                  className="btn btn-sm btn-outline-primary"
-                  onClick={toggleFavorite}
-                  disabled={!isLoggedIn || isFavoritedLoading}
-                >
-                  <i className="ion-heart"></i>{" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-heart-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                    />
-                  </svg>{" "}
-                  {isFavorited ? "Unfavorite Article" : "Favorite Article"}
-                  <span className="counter">({article.favoritesCount})</span>
-                </button>
+                {isLoggedIn && isAuthor ? (
+                  <>
+                    <button
+                      className="btn btn-outline-secondary btn-sm"
+                      onClick={handleEditArticle}
+                    >
+                      <i className="ion-edit"></i>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className ="bi bi-pencil-fill"
+                        viewBox="0 0 16 16"
+                        style={{ marginRight: "3px" }}
+                      >
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                      </svg>
+                      Edit Article
+                    </button>
+                    &nbsp;
+                    <button
+                      className="btn btn-outline-danger btn-sm"
+                      onClick={handleDeleteArticle}
+                    >
+                      <i className="ion-trash-a"></i>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className ="bi bi-trash3-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                      </svg>
+                      Delete Article
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      className="btn btn-sm action-btn btn-outline-secondary"
+                      onClick={toggleFollow}
+                      disabled={!isLoggedIn || isFollowingLoading}
+                    >
+                      <i className="ion-plus-round"></i>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className ="bi bi-plus-lg"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
+                        />
+                      </svg>
+                      {isFollowing ? "Unfollow" : "Follow"}{" "}
+                      {article.author.username}
+                    </button>{" "}
+                    &nbsp;
+                    <button
+                      className="btn btn-sm btn-outline-primary"
+                      onClick={toggleFavorite}
+                      disabled={!isLoggedIn || isFavoritedLoading}
+                    >
+                      <i className="ion-heart"></i>{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-heart-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                        />
+                      </svg>{" "}
+                      {isFavorited ? "Unfavorite Article" : "Favorite Article"}
+                      <span className="counter">
+                        ({article.favoritesCount})
+                      </span>
+                    </button>
+                  </>
+                )}
               </div>
             </Container>
           </div>
@@ -288,7 +365,10 @@ const ArticleDetail = () => {
             </div>
             <hr />
             <div className="row">
-              <div className="than col-xs-12 col-md-8 offset-md-2">
+              <div
+                className="than col-xs-12 col-md-8 offset-md-2"
+                style={{ marginBottom: "100px" }}
+              >
                 {isLoggedIn ? (
                   <form
                     className="card comment-form"
