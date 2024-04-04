4f746c101e9443b3343a57e51be208352b31a86d
NamDD182 <namddhe171565@fpt.edu.vn>
2024-04-04 10:22:48 +0700

    Namm update the tag have underline

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./css/styles.css";
 
 const Sidebar = ({ tagList, handleTagClick }) => {
+  const [selectedTag, setSelectedTag] = useState(null);
+
+  const handleTagSelection = (tag) => {
+    if (selectedTag === tag) {
+      setSelectedTag(null);
+      handleTagClick(null);
+    } else {
+      setSelectedTag(tag);
+      handleTagClick(tag);
+    }
+  };
+
   return (
     <div className="container" style={{ background: '#f3f3f3', borderRadius: '4px', padding: '5px 10px 10px' }}>
       <p>Popular Tags</p>
@@ -11,7 +23,15 @@ const Sidebar = ({ tagList, handleTagClick }) => {
         ) : (
           tagList.tags.map((tag, index) => (
             // eslint-disable-next-line jsx-a11y/anchor-is-valid
-            <a key={index} href="#" className="tag-pill tag-default sidebar-tag" style={{ textDecoration: 'none' }} onClick={() => handleTagClick(tag)}>
+            <a
+              key={index}
+              href="#"
+              className={`tag-pill tag-default sidebar-tag ${
+                selectedTag === tag ? 'selected-tag' : 'unselected-tag'
+              }`}
+              style={{ textDecoration: 'none' }}
+              onClick={() => handleTagSelection(tag)}
+            >
               {tag}
             </a>
           ))
