bd4646e58bc65b5bc857ba157431d4af9f241f6d
Do Hoang Cong Minh <dhcongminh@gmail.com>
2024-04-05 14:00:27 +0700

    remove banner when already login

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -22,7 +22,7 @@ const HomePage = () => {
     API.getTags().then((data) => {
       setTags(data);
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchData = (page, currTag, tag) => {
@@ -88,38 +88,44 @@ const HomePage = () => {
 
   return (
     <div>
-      <div
-        className="banner text-white"
-        style={{
-          display: "inherit",
-          background: "#5CB85C",
-          boxShadow:
-            "inset 0 8px 8px -8px rgba(0, 0, 0, 0.3), inset 0 -8px 8px -8px rgba(0, 0, 0, 0.3)",
-          padding: "2rem",
-          marginBottom: "2rem",
-        }}
-      >
-        <Container>
-          <h1
-            className="logo-font text-white text-center"
-            style={{
-              textShadow: "0px 1px 3px rgba(0, 0, 0, 0.3)",
-              fontSize: "3.5rem",
-              paddingBottom: "0.5rem",
-              fontWeight: "700",
-              marginBottom: "0",
-            }}
-          >
-            conduit
-          </h1>
-          <p
-            className="text-secondary text-white text-center"
-            style={{ fontSize: "1.5rem", fontWeight: "300", marginBottom: "0" }}
-          >
-            A place to share your knowledge.
-          </p>
-        </Container>
-      </div>
+      {!localStorage.getItem("auth-token") && (
+        <div
+          className="banner text-white"
+          style={{
+            display: "inherit",
+            background: "#5CB85C",
+            boxShadow:
+              "inset 0 8px 8px -8px rgba(0, 0, 0, 0.3), inset 0 -8px 8px -8px rgba(0, 0, 0, 0.3)",
+            padding: "2rem",
+            marginBottom: "2rem",
+          }}
+        >
+          <Container>
+            <h1
+              className="logo-font text-white text-center"
+              style={{
+                textShadow: "0px 1px 3px rgba(0, 0, 0, 0.3)",
+                fontSize: "3.5rem",
+                paddingBottom: "0.5rem",
+                fontWeight: "700",
+                marginBottom: "0",
+              }}
+            >
+              conduit
+            </h1>
+            <p
+              className="text-secondary text-white text-center"
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: "300",
+                marginBottom: "0",
+              }}
+            >
+              A place to share your knowledge.
+            </p>
+          </Container>
+        </div>
+      )}
       <Container>
         <div className="row">
           <div className="col-md-9">
